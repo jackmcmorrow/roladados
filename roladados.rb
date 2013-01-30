74c1d7072692a4835c311c992b86ef54ad3e285a
Daniel Cavalcanti <dcc69eyes@gmail.com>
2013-01-30 00:30:16 -0200

    Melhorei a validação do input
    Próximo objetivo: criar uma interface para chamar os outros métodos da
    classe Dados

--- roladados.rb
+++ roladados.rb
@@ -1,4 +1,4 @@
-
+# coding: UTF-8
 #Definindo classe dos objetos Dado, cada dado precisa de um número de faces
 #Ou seja: d6, d8, d12, d20 e por aí vai.
 class Dados
@@ -7,11 +7,14 @@ class Dados
 		@rolagem = 0
 		@total = 0
 	end
-	def rolar (qtd) #incluir uma parâmetro de soma para esse método
+	def rolar (qtd, somar=false) #incluir uma parâmetro de soma para esse método
+
 		1.upto(qtd) do |rolagem| 
 			rolagem = rand(1..@lados)
 			@total += rolagem.to_i
-			puts rolagem
+			unless somar === true
+				puts rolagem
+			end
 		end
 		puts "Total: #{@total}"
 		@total = 0
@@ -51,27 +54,42 @@ _/    _/    _/_/    _/    _/_/_/
 _/_/_/      _/_/_/    _/_/_/    _/_/    _/_/_/
 
 por @cav_dan => www.about.me/cav_dan
-Titulo criado com ASCII Generator => www.network-science.de/ascii
+Título criado com ASCII Generator => www.network-science.de/ascii
 
 INICIO
-puts "Quantas faces tem o dado?"
-faces = STDIN.gets.chomp()
-novoDado = Dados.new(faces.to_i)
+loop do
+	puts "Quantas faces tem o dado?"
+	faces = STDIN.gets.chomp()
+	unless faces.to_i < 1 then
+		@faces = faces
+		NovoDado = Dados.new(faces.to_i)
+		break
+	else
+		puts "Use números, por favor."
+	end
+end
+
 
 puts <<ROLAR
 ******* ROLANDO DADOS *******
 =============================
 ROLAR
-loop {
-puts "Quantos d#{faces} deseja rolar? (ou prefere 'sair'?)"
-quantos = STDIN.gets.chomp()
-quantos === "sair"? break : #Se escrever sair, quebra o loop
-if quantos.to_i < 1
-	puts "Vc precisa rolar um numero"
-else
-puts "Vc rolou %s dados" %quantos.to_i
 
-jogada = novoDado.rolar(quantos.to_i)
-#jogada.maior(quantos.to_i)
-end	
-}
+sair = false
+until sair === true do
+	puts "Quantos d#{@faces} deseja rolar? (ou prefere 'sair'?)"
+	quantos = STDIN.gets.chomp()
+	#Se escrever sair, vai para saída
+	if quantos === "sair"
+		sair = true
+	else
+		#Ruby retorna 0 se não conseguir transformar string em integer
+		if quantos.to_i < 1 
+			puts "Você precisa rolar um numero"
+		else
+			puts "Você rolou %s dados" %quantos.to_i
+			jogada = NovoDado.rolar(quantos.to_i)
+			#jogada.maior(quantos.to_i)
+		end
+	end
+end
